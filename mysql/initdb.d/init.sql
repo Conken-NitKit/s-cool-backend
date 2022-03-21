dda48f8e9794b195c0775184104cf09dc66fbd6d
shu20031026 <shu20031026@gmail.com>
2022-03-21 17:50:29 +0900

    add: mysqlの立ち上げ

---
+++ mysql/initdb.d/init.sql
@@ -0,0 +1,19 @@
+-- -- 初期処理
+-- DROP DATABASE IF EXISTS testdb;
+-- CREATE DATABASE testdb;
+-- USE testdb;
+-- DROP TABLE IF EXISTS test;
+
+-- CREATE TABLE test
+-- (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   name TEXT NOT NULL
+-- )DEFAULT CHARACTER
+--   SET=utf8;
+
+--   INSERT INTO test
+--     (name)
+--   VALUES
+--     ("窪田"),
+--     ("林"),
+--     ("野口");
